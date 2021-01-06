510d82db0e2fd6b0f6ebc9b42abfac9f920eb4f7
Danielpg <daniieelpg12@gmail.com>
2021-01-06 17:14:09 -0500

    programa terminado

--- src/fairsquare/FairSquare.java
+++ src/fairsquare/FairSquare.java
@@ -4,58 +4,47 @@ package fairsquare;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class FairSquare {
 
- 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new FileReader("entrada.txt"));
-        int Firstline = Integer.parseInt(bf.readLine());
+        int Firstline = Integer.parseInt(bf.readLine()); //primera linea del archivo = numero de casos
         for(int i=0;i<Firstline;i++){
-            int justo_Y_Cuadrado = 0;
+            int justoC = 0;
             String linea = bf.readLine();
             String[] arr = linea.split(" ");
             int A = Integer.parseInt(arr[0]);
             int B = Integer.parseInt(arr[1]);
-            Recorrido(A,B,justo_Y_Cuadrado);
-        }
-
-    }
-    public static void Recorrido(int A,int B,int justoC){
-        while(A<=B){
-            
-            A++;
-        }
-    }
-    public static void JustoYCuadrado(int n, int justoC) {
-        if (Palindromo(String.valueOf(n))) {
-            System.out.println("es palindromo");
-            String resultado = String.valueOf(Math.sqrt(n));
-            int posicionPunto = 0;
-            for (int j = 0; j < resultado.length(); j++) {
-                if (resultado.charAt(j) == '.') {
-                    //int Numero=Integer.parseInt(resultado.charAt(j));
-                    posicionPunto = resultado.indexOf('.');
-                }
+            while(A<=B){
+                justoC=(JustoYCuadrado(A,justoC));
+                A++;
             }
-            String[] arreglo = resultado.split("");
-            int Numero = Integer.parseInt(arreglo[posicionPunto + 1]);
-
+            System.out.println("Case #"+(i+1)+": "+justoC);
+        }
+    }
+    public static int JustoYCuadrado(int n, int justoC) {
+        if (Palindromo(String.valueOf(n))){
+            //System.out.println("es palindromo");
+            String resultado = String.valueOf(Math.sqrt(n)); //la raiz cuadrada del numero
+            int posicionPunto = 0; //posicion del punto e la cadena
+            for (int j = 0; j < resultado.length(); j++) {
+                if (resultado.charAt(j) == '.') posicionPunto = resultado.indexOf('.');
+            }
+            String[] arreglo = resultado.split(""); //separar la cadena
+            int Numero = Integer.parseInt(arreglo[posicionPunto + 1]); //numero a evaluar despues del punto
             if (ValidarRaizExacta(Numero)) {
-                System.out.println("Es cuadrado");
+                //System.out.println("Es cuadrado tiene raiz exacta");
                 double raiz = Math.sqrt(n);
                 if (Palindromo(String.valueOf(Math.round(raiz)))) {
-                    System.out.println("El cuadrado es palindromo");
+                    //System.out.println("El cuadrado es palindromo");
                     justoC++;
                 }
             }
         }
+        return justoC;
     }
     public static boolean Palindromo(String n){
-        System.out.println(n);
         String[]array=n.split("");
         int i=0,j=array.length;
         int Coincide=0,NoCoincide=0;
@@ -69,37 +58,11 @@ public class FairSquare {
             j--;
         }
         if(Coincide>NoCoincide)return true;
-        
         return false;
-        
     }
-    
     public static boolean ValidarRaizExacta(int N){
         boolean b=true;
-        if(N!=0){
-            b=false;
-        }
+        if(N!=0)b=false;
         return b;
     }
-    
-    
-     //System.out.println(posicionPunto);
-        
-        
-        //System.out.println(resultado);
-        /*double r=Math.sqrt(n);
-        if((r%2)!=0){
-            System.out.println("Resultado decimal No es Cuadrado");
-        }*/
-        /*Pattern P = Pattern.compile("^([.][0-9])$");//^[0-9]+([.][0-9]+)?$
-        Matcher M = P.matcher("5.0");
-        if(M.matches()){
-            System.out.println("tiene numeros decimales");
-        }*/
-        
-        //for(int h=posicionPunto+1;h<resultado.length();h++){
-            //System.out.println(resultado);
-            //Pattern P = Pattern.compile(resultado);
-       // }
-        //}
 }
